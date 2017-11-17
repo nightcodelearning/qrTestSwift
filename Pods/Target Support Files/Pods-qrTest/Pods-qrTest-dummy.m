1c947bca00a0ba07194cbc788c984f317cb31000
nightcodelearning <nightcodelearning@gmail.com>
2017-11-16 21:46:39 -0500

    aschuch_QRCode example for swift 3

---
+++ Pods/Target Support Files/Pods-qrTest/Pods-qrTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_qrTest : NSObject
+@end
+@implementation PodsDummy_Pods_qrTest
+@end
--- qrTest/ViewController.swift
+++ qrTest/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import QRCode
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image_outlet: UIImageView!
+    @IBOutlet weak var textOutlet: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func generateQR(_ sender: UIButton) {
+        // String
+        if textOutlet.text != ""{
+            var qrCode = QRCode(textOutlet.text!)
+            //        qrCode?.image
+            qrCode?.size = CGSize(width: 300, height: 300)
+            qrCode?.color = CIColor(rgba: "D65442")
+            qrCode?.backgroundColor = CIColor(rgba: "FFFFFF")
+            let imageView = UIImageView(qrCode: qrCode!)
+            
+            image_outlet.image = imageView.image
 
+        }
+        
+    }
+    
 }
 
